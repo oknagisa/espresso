1ec1e42de9a7dc81c81989ec366362a9d24993fc
rs40416996@gmail.com <rs40416996@gmail.com>
2020-06-18 19:44:48 +0530

    Basic local caching for search results

--- js/data-layer/search.js
+++ js/data-layer/search.js
@@ -1,9 +1,20 @@
 import {getData} from './dataIO';
 
+const cache = {};
+
+const filter = (key, data) => {
+  if (!cache[key]) {
+    cache[key] = data.filter(ob => ob.name.toLowerCase().includes(key));
+    setTimeout(() => delete cache[key], 60000);
+    console.log('Updating cache: ', cache);
+  }
+  return cache[key];
+};
+
 export const search = searchText =>
   getData().then(
     data =>
       new Promise(res =>
-        res(searchText ? data.filter(ob => ob.name.includes(searchText)) : [])
+        res(searchText ? filter(searchText.toLowerCase(), data) : [])
       )
   );
