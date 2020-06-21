5c74b542ff15c22ff118bbacba40911b9f1ae9b7
Snehalkurkute <snehalkurkute2@gmail.com>
2020-06-21 20:20:42 +0530

    Country Flag on card

--- js/components/Card/card.js
+++ js/components/Card/card.js
@@ -1,45 +1,55 @@
 import React from 'react';
-import {StyleSheet, TextInput, View, Image, Text} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 import ReadMore from 'react-native-read-more-text';
-const Card = ({name, country, description}) => {
-  return (
-    <View style={styles.card}>
-      <Text style={styles.brandName}>{name}</Text>
-      <Text style={styles.country}>{country}</Text>
-      <ReadMore numberOfLines={2}>
-        <Text>{description}</Text>
-      </ReadMore>
-    </View>
-  );
+import Flag from 'react-native-flags';
+
+const Card = ({name, country, description, flag}) => {
+    return (
+        <View style={styles.card}>
+            <Text style={styles.brandName}>{name}</Text>
+            <View style={styles.countryContainer}>
+                <Text style={styles.country}>{country}</Text>
+                {flag ? <Flag style={styles.flag} code={flag} size={32}/> : null }
+            </View>
+            <ReadMore numberOfLines={2}>
+                <Text>{description}</Text>
+            </ReadMore>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  card: {
-    margin: 20,
-    padding: 20,
-    fontSize: 16,
-    borderRadius: 30,
-    backgroundColor: '#fff',
-    borderColor: '#fff',
-    borderWidth: 1,
-    shadowColor: '#fff',
-    shadowOffset: {
-      width: 0,
-      height: 2
+    card: {
+        margin: 20,
+        padding: 20,
+        fontSize: 16,
+        borderRadius: 30,
+        backgroundColor: '#fff',
+        borderColor: '#fff',
+        borderWidth: 1,
+        shadowColor: '#fff',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+        elevation: 7
     },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-    elevation: 7
-  },
-  brandName: {
-    fontSize: 25,
-    lineHeight: 35
-  },
-  country: {
-    fontSize: 20,
-    lineHeight: 35
-  },
-  basicInfo: {}
+    brandName: {
+        fontSize: 25,
+        lineHeight: 35
+    },
+    countryContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    flag: {},
+    country: {
+        fontSize: 20,
+        lineHeight: 35,
+    },
+    basicInfo: {}
 });
 
 export default Card;
