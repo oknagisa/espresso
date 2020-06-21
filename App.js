00eed90a4528338d54b686209fb5fd8dbf250cbd
Snehalkurkute <snehalkurkute2@gmail.com>
2020-06-21 22:28:43 +0530

    spinner

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, ActivityIndicator} from 'react-native';
 import {useInitData} from './js/hooks/useInitData';
 import HomePage from './js/components/home/home-page';
 
 const App = () => {
-  // Load main app data
-  const {loading} = useInitData();
-  return loading ? <Text> Loading...</Text> : <HomePage />;
+    // Load main app data
+    const {loading} = useInitData();
+    return loading ? <ActivityIndicator size="small" color="#123085" style={styles.loading}/> : <HomePage/>;
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    loading: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }
+});
 
 export default App;
